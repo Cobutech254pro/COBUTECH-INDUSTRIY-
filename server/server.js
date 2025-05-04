4e765877e51d19b6fd35d383b785aee4218e7542
Cobutech254pro <calvinonyango963@gmail.com>
2025-05-04 14:36:24 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,12 +3,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const admin = require('firebase-admin');
 const authRoutes = require('./routes/authRoutes'); // Assuming this is the path to your auth routes
+const path = require('path'); // Import the 'path' module
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, '..', 'public'))); // Adjust path as needed
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
@@ -18,13 +22,12 @@ mongoose.connect(process.env.MONGODB_URI, {
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Could not connect to MongoDB:', err));
 
-// Initialize Firebase Admin SDK (Make sure you have the credentials set as an environment variable)
+// Initialize Firebase Admin SDK
 let serviceAccount;
 try {
   serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_CREDENTIALS);
 } catch (error) {
   console.error('Error parsing FIREBASE_ADMIN_CREDENTIALS:', error);
-  // Handle the error appropriately, maybe exit the process if Firebase is essential
 }
 
 if (serviceAccount) {
@@ -37,6 +40,11 @@ if (serviceAccount) {
 
 // Use authentication routes
 app.use('/api/auth', authRoutes);
+
+// Handle all other requests by serving the index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html')); // Adjust path as needed
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
