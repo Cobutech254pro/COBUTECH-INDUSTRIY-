01659bc8cf2cd44e1e1cc005449daf515a761c54
Cobutech254pro <calvinonyango963@gmail.com>
2025-05-04 19:52:45 +0300

    𝐏𝐑𝐄𝐌𝐈𝐔𝐌

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,13 +2,9 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-// Define the sign-up route
 router.post('/signup', authController.signup);
-
-// Define the route to send verification code
+router.post('/login', authController.login); // Add the login route
 router.post('/send-verification-code', authController.sendVerificationCode);
-
-// Define the route to verify the code
 router.post('/verify-code', authController.verifyCode);
 
 module.exports = router;
