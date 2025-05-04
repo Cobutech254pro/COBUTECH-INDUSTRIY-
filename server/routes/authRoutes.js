80419614a885762d7ea9e9efba3b19d33323986e
Cobutech254pro <calvinonyango963@gmail.com>
2025-05-04 12:41:16 +0300

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,6 +5,4 @@ const authController = require('../controllers/authController');
 // Define the sign-up route
 router.post('/signup', authController.signup);
 
-// You'll add other authentication routes here later (e.g., /login)
-
 module.exports = router;
