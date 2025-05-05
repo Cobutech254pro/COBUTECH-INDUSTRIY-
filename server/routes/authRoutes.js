41f1e9baab70359612345549113434d8fb22b1c9
Cobutech254pro <calvinonyango963@gmail.com>
2025-05-05 08:47:15 +0300

    𝐏𝐑𝐄𝐌𝐈𝐔𝐌

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -6,5 +6,6 @@ router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/send-verification-code', authController.sendVerificationCode);
 router.post('/verify-code', authController.verifyCode);
+router.post('/forgot-password', authController.forgotPassword); // Add this route
 
 module.exports = router;
