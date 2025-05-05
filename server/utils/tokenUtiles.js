703e2495b1565fb8b6dc93a318c75e41954e69d1
Cobutech254pro <calvinonyango963@gmail.com>
2025-05-05 17:50:08 +0300

    𝐏𝐑𝐄𝐌𝐈𝐔𝐌

--- server/utils/tokenUtiles.js
+++ server/utils/tokenUtiles.js
@@ -1,9 +1,8 @@
 const jwt = require('jsonwebtoken');
-
-const JWT_SECRET = process.env.JWT_SECRET || 'your-secret-key'; // Use environment variable for security
+const JWT_SECRET = process.env.JWT_SECRET || 'your-secret-key';
 
 const generateToken = (userId) => {
-  return jwt.sign({ userId }, JWT_SECRET, { expiresIn: '1h' }); // You can adjust the expiration time as needed
+  return jwt.sign({ userId }, JWT_SECRET, { expiresIn: '1h' });
 };
 
 module.exports = { generateToken };
